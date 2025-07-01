3538d52eeb2d48d3d087c5d6ff6dd2565af4526f
henrique vera <hverar@gmail.com>
2025-07-01 12:28:46 -0400

    searchbar and log in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,18 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :brands, :music, :events, :advertise, :searchbar, :discover]
 
   def home; end
-  def brands; end
-  def music; end
 
   def events
     @events = MtlEvent.all
   end
 
   def advertise; end
-  def searchbar; end
 
-  def discover
-  end
+  def discover; end
 
   def brands
     @brands = Brand.all
@@ -23,5 +19,24 @@ class PagesController < ApplicationController
     @musics = Music.all
   end
 
+  def searchbar
+    @query = params[:query]
+
+    if @query.present?
+      @products = Product.where("name ILIKE ?", "%#{@query}%")
+      @brands   = Brand.where("name ILIKE ?", "%#{@query}%")
+      @events   = MtlEvent.where("name ILIKE ?", "%#{@query}%")
+      @songs    = Song.where("title ILIKE ?", "%#{@query}%")
+      @artists  = Artist.where("name ILIKE ?", "%#{@query}%")
+      @albums   = Music.where("title ILIKE ?", "%#{@query}%")
+    else
+      @products = []
+      @brands = []
+      @events = []
+      @songs = []
+      @artists = []
+      @albums = []
+    end
+  end
 
 end
