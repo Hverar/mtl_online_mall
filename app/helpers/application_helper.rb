28ceb2c0d42b241611a07e2eaccded59005f1c8a
henrique vera <hverar@gmail.com>
2025-07-05 00:44:31 -0400

    searchbar and cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def hide_navbar?
+    (
+      devise_controller? && controller_name.in?(%w[sessions registrations passwords])
+    ) || (
+      controller.controller_name == "pages" && controller.action_name == "searchbar"
+    )
+  end
 end
